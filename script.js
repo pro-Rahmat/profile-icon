73bf1973485d6600048636a2173e05b138a21d57
pro-Rahmat <153966402+pro-Rahmat@users.noreply.github.com>
2024-01-09 10:48:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function accountMenu(){
+    var element = document.getElementById('account__menu');
+    element.classList.remove('hidden');
+}
+function accountMenuHidder(){
+    var element = document.getElementById('account__menu');
+    element.classList.add('hidden');
+}
+function accountManagement(){
+    var element = document.getElementById('account__management');
+    element.classList.remove('hidden');
+}
+function accountManagementHidder(){
+    var element = document.getElementById('account__management');
+    element.classList.add('hidden');
+}
+function interestsEdit(){
+    var element = document.getElementById('interests-edit');
+    element.classList.remove('hidden');
+}
+function interestsEditHidder(){
+    var element = document.getElementById('interests-edit');
+    element.classList.add('hidden');
+}
